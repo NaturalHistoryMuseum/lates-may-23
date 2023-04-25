ee02bf8efd59e60c2d0a9412f6f9ac842e658c1d
Ginger Butcher <alycejenni@gmail.com>
2023-04-25 11:30:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## How to add images
+
+[UPLOAD PAGE HERE](https://github.com/NaturalHistoryMuseum/lates-may-23/upload/main/images)
+
+If that link doesn't work:
+["images" folder](/images) > "Add file" button > "Upload files"
+
+1. Upload any new images from this interface
+2. Click "commit changes"
+3. Wait for about a minute
+4. The new pictures should appear on [the site](https://naturalhistorymuseum.github.io/lates-may-23)
+
+***
+
+## Troubleshooting
+
+If something went wrong, it's almost certainly in the [actions](https://github.com/NaturalHistoryMuseum/lates-may-23/actions) (automated processes that rebuild/deploy the site when new things are uploaded).
+
+- check the symbols:
+  - yellow dots mean it's still processing
+  - red crosses mean something went wrong and you'll probably have to ask Ginger
+  - grey circles with exclamation marks mean it was skipped, which is _probably_ fine
+  - green ticks are all good
+- check the times on the right hand side:
+  - if the latest one doesn't match up with when you uploaded the files (roughly), it may not have started running for some reason
+  - wait until you upload something else, and if it doesn't work again then ask Ginger
---
+++ src/lates.py
@@ -0,0 +1,52 @@
+# !/usr/bin/env python
+# encoding: utf-8
+
+from jinja2 import Environment, PackageLoader, select_autoescape
+import os
+import shutil
+
+
+env = Environment(
+    loader=PackageLoader('lates'),
+    autoescape=select_autoescape()
+)
+
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+input_dir = os.path.dirname(__file__)
+
+output_dir = os.path.join(base_dir, 'docs')
+
+
+def copy_content(input_content_dir, output_content_dir, replace=True):
+    input_content = os.listdir(input_content_dir)
+    if not os.path.exists(output_content_dir):
+        os.mkdir(output_content_dir)
+    output_content = os.listdir(output_content_dir)
+    for item in input_content:
+        if item not in output_content or replace:
+            shutil.copy(os.path.join(input_content_dir, item), os.path.join(output_content_dir, item))
+
+
+def get_images():
+    input_images_dir = os.path.join(base_dir, 'images')
+    output_images_dir = os.path.join(output_dir, 'images')
+    copy_content(input_images_dir, output_images_dir)
+    return os.listdir(output_images_dir)
+
+
+def get_styles():
+    input_styles_dir = os.path.join(input_dir, 'styles')
+    output_styles_dir = os.path.join(output_dir, 'styles')
+    copy_content(input_styles_dir, output_styles_dir)
+    return os.listdir(output_styles_dir)
+
+
+def render_page():
+    images = get_images()
+    styles = get_styles()
+    template = env.get_template('index.html')
+    template.stream(images=images, styles=styles).dump(os.path.join(output_dir, 'index.html'))
+
+
+render_page()
